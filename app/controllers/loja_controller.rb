ca68258f0901011d72c251b76c2fadfabcd70c97
Jota Cervan <jota@mobile2you.com.br>
2016-12-06 18:46:20 -0200

    Teste

--- app/controllers/loja_controller.rb
+++ app/controllers/loja_controller.rb
@@ -7,6 +7,9 @@ class LojaController < ApplicationController
   	@states = Store.distinct(:state)
   	@selected = params[:state]
   	@neighborhood = Store.where(:state => params[:state])
+    #Session definition start
+    session[:state] = @selected
+    #Session definition end
   end
 
   def cardapio
@@ -17,28 +20,95 @@ class LojaController < ApplicationController
   	@neighborhood = Store.where(:state => params[:state])
   	@store = Store.find(@id)
     @size = params[:size]
+    #Session definition start
+    session[:state] = @selected
+    session[:neighbor] = @neighbor
+    session[:id] = @id
+    session[:size] = @size
+    #Session definition end
   end
-
+  
   def login
+    consult = RestClient.post 'http://pizzaprime.herokuapp.com/webservices/login/signin',  {  email: params[:login][:email], password: params[:login][:password]  }, { 'content-type' => 'application/json' }
 
-    RestClient.post('http://pizzaprime.herokuapp.com/webservices/login/signin',  {  email: params[:login][:email], password: params[:login][:password]  }){ |response, request, result, &block|
-        
-        cookies[:session_id] = response.cookies['_session_id']
-        redirect_to pizzas_path
-        #render json: response.cookies['_session_id']
+    cookies[:session_id] = consult.cookies['_session_id']
+    
+    resultado = JSON.parse(consult.body)
+    session[:logged] = true
+    session[:name] = resultado['name']
+    session[:email] = resultado['email']
+    session[:picture] = resultado['picture']
+    session[:gender] = resultado['gender']
+    session[:facebook] = resultado['facebook']
+    session[:balance] = resultado['balance']
+    session[:phone] = resultado['phone']
+    session[:cpf] = resultado['cpf']
 
-    }
-
+    redirect_to cardapio_path(session[:state],session[:neighbor],session[:id],session[:size])
+    
   end
   
   def pizzas
-
     RestClient.get('http://pizzaprime.herokuapp.com/webservices/stores/getStores', {  :cookies => { '_session_id' => cookies[:session_id] }  } ){ |response, request, result, &block|
-        
         render json: response
+    }
+  end
+
+  def cart
+
+  end
+
+  def modeljson
+
+    pedido = { 
+
+      :pedido => {
+
+        :pizzas => {
+          :size_id => '2222',
+          :border_id => '11111',
+          :quantity => 1,
+          :integral => true,
+          :obs => 'Observações',
+          :fidelity => true,
+          :tastes => {
+            :id => '123123',
+            :id => '123123'
+          }
+        },
+
+        :sweet_pizzas => {
+          :size_id => '2222',
+          :quantity => 1,
+          :obs => 'Observações',
+          :integral => true,
+          :fidelity => true,
+          :tastes => {
+            :id => '123123',
+            :id => '123123'
+          }
+        },
+
+        :beverages => {
+          :id => '123',
+          :quantity => 1,
+          :fidelity => true
+        },
+
+        :combos => {
+          :id => '1234',
+          :quantity => 1,
+          :pizzas => {},
+          :sweet_pizzas => {},
+          :beverages => {}
+        }
+
+      }
 
     }
-    
+
+    render json: pedido
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   	get 'cardapio/:state/:neighbor/:id/:size' => 'loja#cardapio', as: :cardapio
   	post 'login' => 'loja#login', as: :login
   	get 'pizzas' => 'loja#pizzas', as: :pizzas
-
+  	get 'modeljson' => 'loja#modeljson', as: :modeljson
+  	get 'cart' => 'loja#cart', as: :cart
+  	
     get 'home' => 'home#index', as: :home
 
     root 'home#index'
